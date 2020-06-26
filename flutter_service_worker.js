8f9a994f07486a2676ec1afb0242f43145017fe6
Shamik <admin@example.com>
2020-06-26 10:00:08 +0530

    Without device preview

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,26 +3,23 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2d34edbc240762faf1fe75457221bcbc",
+  "assets/AssetManifest.json": "ad808460338efe404a6134b95e0e028a",
 "assets/assets/images/a_1_1.jpeg": "46daa1bfebfe15618ab31bc5e689462b",
 "assets/assets/images/a_1_2.jpeg": "007d61389c8abf98cf98e791404b3d88",
 "assets/assets/images/a_1_3.jpeg": "62c89a062e8c88e8bd0202fec3addcd4",
 "assets/assets/images/a_1_4.jpeg": "a403e7515b8a47fc95507a59ddd38366",
 "assets/assets/images/header_logo.png": "28562c09ea9372a30e37d669144c56e3",
 "assets/assets/images/q_1.jpeg": "71f04df0bf52743c4285f0ec721cff13",
-"assets/FontManifest.json": "18eda8e36dfa64f14878d07846d6e17f",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/NOTICES": "9b8e82531a2b5920f88aa44be3c93c8c",
+"assets/NOTICES": "fa12b98431c8047df4208e4c7ddd631d",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
-"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "7fd068f895b0232f9bd1f860f846c0fd",
 "/": "7fd068f895b0232f9bd1f860f846c0fd",
-"main.dart.js": "0a9f4f65e7809cb96488a78ed709402c",
+"main.dart.js": "0a6ca019908caf9e85807f852dcecbc6",
 "manifest.json": "95f9a83e89825c013b43abd1a3f3169c"
 };
 
